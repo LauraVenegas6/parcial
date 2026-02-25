fba09e259f57a496475977b445381c74f80d17a1
unknown <laura.venegas-p@labinfo.is.escuelaing.edu.co>
2026-02-25 18:45:11 -0500

    Punto 1: e crea la clase PiDigitsThread donde se calcula la funcion BBP

---
+++ src/main/java/edu/eci/arsw/math/PiDigitsThread.java
@@ -0,0 +1,53 @@
+package edu.eci.arsw.math;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class PiDigitsThread extends Thread {
+    private int start;
+    private int end; 
+    private List<Integer> digits;
+
+    public static final Object lock = new Object();
+
+    public PiDigitsThread(int start, int end, List<Integer> digits){
+        this.start = start;
+        this.end = end;
+        this.digits = new ArrayList<>();
+    }
+    
+    @Override
+    public void run() {
+        formulaBBP(start, end);
+    }
+
+    private void formulaBBP(int start, int end) {
+
+        for (int n = start; n < end; n++) {
+            double sum = 0.0;
+            for (int k = 0; k <= n; k++) {
+                sum += (1.0 / Math.pow(16, k)) * 
+                       ((4.0 / (8 * k + 1)) - 
+                        (2.0 / (8 * k + 4)) - 
+                        (1.0 / (8 * k + 5)) - 
+                        (1.0 / (8 * k + 6)));
+            }
+            int digit = (int) ((sum - Math.floor(sum)) * 16);
+            digits.add(digit);
+        }
+    }
+
+    public List<Integer> getDigits() {
+        return digits;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+
+    }
+}
+    
